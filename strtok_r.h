e7210c3c30612d67d2c6e3f18c9e842d788ec746
Ozkan Sezer <sezeroz@gmail.com>
2020-11-05 22:40:20 +0300

    build fixes and updates:
    - actually use cmake TEST_BIG_ENDIAN result: replace the
      __BYTE_ORDER == __LITTLE_ENDIAN check in the source with
      !defined(WORDS_BIGENDIAN).
    - refresh strtok_r.h from the CC0 licensed PDClib library
      at https://github.com/DevSolar/pdclib.git
    - add strtok_r() check to cmake and adjust sources for it.
    - makefile adjustments.

--- strtok_r.h
+++ strtok_r.h
@@ -1,39 +1,70 @@
-/* This file is in the public domain */
+/*
+ * Adapted from _PDCLIB_strtok() of PDClib library at
+ * https://github.com/DevSolar/pdclib.git
+ *
+ * The code was under CC0 license:
+ * https://creativecommons.org/publicdomain/zero/1.0/legalcode :
+ *
+ *                        No Copyright
+ *
+ * The person who associated a work with this deed has dedicated the
+ * work to the public domain by waiving all of his or her rights to
+ * the work worldwide under copyright law, including all related and
+ * neighboring rights, to the extent allowed by law.
+ *
+ * You can copy, modify, distribute and perform the work, even for
+ * commercial purposes, all without asking permission. See Other
+ * Information below.
+ */
 
-#include <string.h>
+#include <stddef.h> /* for NULL */
 
-static char *
-unsf_strtok_r(char *str, const char *delim, char **saveptr)
+static char *unsf_strtok_r(char *s1, const char *s2, char **ptr)
 {
-   char *p;
+    const char *p = s2;
 
-   if (str == NULL)
-      str = *saveptr;
+    if (!s2 || !ptr || (!s1 && !*ptr)) return NULL;
 
-   if (str == NULL)
-      return NULL;
+    if (s1 != NULL) {  /* new string */
+        *ptr = s1;
+    } else { /* old string continued */
+        if (*ptr == NULL) {
+        /* No old string, no new string, nothing to do */
+            return NULL;
+        }
+        s1 = *ptr;
+    }
 
-   while (*str && strchr(delim, *str))
-      str++;
+    /* skip leading s2 characters */
+    while (*p && *s1) {
+        if (*s1 == *p) {
+        /* found separator; skip and start over */
+            ++s1;
+            p = s2;
+            continue;
+        }
+        ++p;
+    }
 
-   if (*str == '\0')
-   {
-      *saveptr = NULL;
-      return NULL;
-   }
+    if (! *s1) { /* no more to parse */
+        *ptr = s1;
+        return NULL;
+    }
 
-   p = str;
-   while (*p && !strchr(delim, *p))
-      p++;
+    /* skipping non-s2 characters */
+    *ptr = s1;
+    while (**ptr) {
+        p = s2;
+        while (*p) {
+            if (**ptr == *p++) {
+            /* found separator; overwrite with '\0', position *ptr, return */
+                *((*ptr)++) = '\0';
+                return s1;
+            }
+        }
+        ++(*ptr);
+    }
 
-   if (*p == '\0')
-      *saveptr = NULL;
-   else
-   {
-      *p = '\0';
-      p++;
-      *saveptr = p;
-   }
-
-   return str;
+    /* parsed to end of string */
+    return s1;
 }
